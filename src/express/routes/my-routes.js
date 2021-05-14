fa5f434042284abee797cff1b1376f920641fac2
barkova <barkova.kate@yandex.ru>
2021-05-14 18:16:01 +0300

    refs #module3-task2

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,8 +3,8 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.send(`/my`));
-myRouter.get(`/comments`, (req, res) => res.send(`my/comments`));
+myRouter.get(`/`, (req, res) => res.send(`comments`));
+myRouter.get(`/comments`, (req, res) => res.send(`my`));
 
 
 module.exports = myRouter;
