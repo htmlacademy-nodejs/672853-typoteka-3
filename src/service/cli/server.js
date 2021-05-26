8e4c461d582ce6b367278a063299ba7ec2bb1ee9
barkova <barkova.kate@yandex.ru>
2021-05-26 13:44:23 +0300

    refs  #module3-task3 *add express to cli

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,65 +1,46 @@
 'use strict';
 
 const chalk = require(`chalk`);
-const http = require(`http`);
+const express = require(`express`);
+
 const fs = require(`fs`).promises;
 const {HttpCode, DEFAULT_PORT, MOCKS_SOURCE, NOT_FOUND_MESSAGE_TEXT} = require(`../../constants`);
 
-const getTemplate = (message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  return template;
-};
-
-const sendResponse = (res, statusCode, template, type = `text/html; charset=UTF-8`) => {
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': type,
-  });
-
-  res.end(template);
-};
+const app = express();
+app.use(express.json());
 
 const prepareData = async () => {
-  const fileContent = await fs.readFile(MOCKS_SOURCE);
-  const mocks = JSON.parse(fileContent);
-  const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-  return getTemplate(`<ul>${message}</ul>`);
-};
-
-const onClientConnect = async (req, res) => {
-  switch (req.url) {
-    case `/`:
-      try {
-        const template = await prepareData();
-        sendResponse(res, HttpCode.OK, template);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, getTemplate(NOT_FOUND_MESSAGE_TEXT));
-      }
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, getTemplate(NOT_FOUND_MESSAGE_TEXT));
-      break;
+  try {
+    const fileContent = await fs.readFile(MOCKS_SOURCE);
+    const mocks = JSON.parse(fileContent);
+    return mocks;
+  } catch (err) {
+    return [];
   }
 };
 
+
+app.get(`/posts`, async (req, res) => {
+  try {
+    const mocks = await prepareData();
+    res.json(mocks);
+  } catch (err) {
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+  }
+});
+
+app.use((req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(NOT_FOUND_MESSAGE_TEXT));
+
 const createServer = (port) => {
-  http.createServer(onClientConnect)
-    .listen(port)
-    .on(`listening`, (err) => {
-      if (err) {
-        console.error(chalk.red(`Ошибка при создании сервера`), err);
-      } else {
-        console.info(chalk.green(`Ожидаю соединений на ${port}`));
-      }
-    });
+  app.listen(port, (err) => {
+    if (err) {
+      console.error(chalk.red(`Ошибка при создании сервера`), err);
+    } else {
+      console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    }
+  });
 };
 
 module.exports = {
