4a980d10b678cae2dc2b6644ca569d7a9c346ca5
Barkova <barkova.kate@yandex.ru>
2021-06-20 22:30:26 +0300

    refs #module5-task1

---
+++ schema.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE typoteka
+  WITH
+  OWNER = postgres
+  ENCODING = 'UTF8'
+  CONNECTION LIMIT = -1;
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS articles CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS articles_categories;
+DROP TABLE IF EXISTS comments;
+
+CREATE TABLE users(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  email varchar(255) UNIQUE NOT NULL,
+  password_hash varchar(255) NOT NULL,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  avatar varchar(50) NOT NULL
+);
+
+CREATE TABLE articles(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title text NOT NULL,
+  announce TEXT NOT NULL,
+  full_text TEXT,
+  image TEXT,
+  created_date timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE categories(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name varchar(255) NOT NULL
+);
+
+CREATE TABLE articles_categories(
+  articles_id integer NOT NULL,
+  category_id integer NOT NULL,
+  PRIMARY KEY (articles_id, category_id),
+  FOREIGN KEY (articles_id) REFERENCES articles(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  FOREIGN KEY (articles_id) REFERENCES categories(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE comments(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  article_id integer NOT NULL,
+  user_id integer NOT NULL,
+  text text NOT NULL,
+  created_date timestamp DEFAULT current_timestamp,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  FOREIGN KEY (article_id) REFERENCES articles(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE INDEX ON articles(title);
